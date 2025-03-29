957da30c33aad5f708d58cdda4478f752d1c8c5d
Gustavo de Assis Souza <gustavoassissouza2016@gmail.com>
2025-03-29 15:47:26 -0300

    Vou sair, chegando em casa termino (TO QUASE LÁ)

--- src/App.java
+++ src/App.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         String nome, viagemExterior, contatoSintomas, sintomas;
-        int idade, probabilidadeInfeccao = 0;
+        int idade, probabilidadeInfeccao = 0,tentativas = 3;
 
         Scanner scanner = new Scanner(System.in);
         while (true) { 
-            System.out.print("Informe o seu nome: ");
+            System.out.println("Informe o seu nome:");
             nome = scanner.next();
-            System.out.print("Informe a sua idade:");
+            System.out.println("Informe a sua idade:");
             idade = scanner.nextInt();
 
-            while (true){ // Loop para perguntar os sintomas
+            while (tentativas > 0){ // Loop para perguntar os sintomas
                 System.out.println("Responda com 'SIM' ou 'NAO'");
+                System.out.println("Não responda errado, você tem apenas %d tentativas".formatted(tentativas));
                 System.out.println("Seu cartão de vacina está em dia?");
                 String cartaoVacina = scanner.next();
                 if (!cartaoVacina.equalsIgnoreCase("SIM") && !cartaoVacina.equalsIgnoreCase("NAO")){
-                    int tentativas = 4;
-                    if (tentativas < 1){
-                        break;
-                    }
                     System.out.println("Responda APENAS com 'SIM' ou 'NAO'. Você tem apenas %d tentativas".formatted(tentativas));
                     tentativas--;
                     continue;
@@ -31,10 +28,6 @@ public class App {
                 System.out.println("Teve algum dos sintomas recentemente? (dor de cabeça, febre, náusea, dor articular, gripe)");
                 sintomas = scanner.next();
                 if (!sintomas.equalsIgnoreCase("SIM") && !sintomas.equalsIgnoreCase("NAO")){
-                    int tentativas = 4;
-                    if (tentativas < 1){
-                        break;
-                    }
                     System.out.println("Responda APENAS com 'SIM' ou 'NAO'. Você tem apenas %d tentativas".formatted(tentativas));
                     tentativas--;
                     continue;
@@ -44,10 +37,6 @@ public class App {
                 System.out.println("Teve contato com pessoas com sintomas gripais nos últimos dias?");
                 contatoSintomas = scanner.next();
                 if (!contatoSintomas.equalsIgnoreCase("SIM") && !contatoSintomas.equalsIgnoreCase("NAO")){
-                    int tentativas = 4;
-                    if (tentativas < 1){
-                        break;
-                    }
                     System.out.println("Responda APENAS com 'SIM' ou 'NAO'. Você tem apenas %d tentativas".formatted(tentativas));
                     tentativas--;
                     continue;
@@ -57,28 +46,47 @@ public class App {
                 System.out.println("Está retornando de viagem realizada no exterior?");
                 viagemExterior = scanner.next();
                 if (!viagemExterior.equalsIgnoreCase("SIM") && !viagemExterior.equalsIgnoreCase("NAO")){
-                    int tentativas = 4;
-                    if (tentativas < 1){
-                        break;
-                    }
                     System.out.println("Responda APENAS com 'SIM' ou 'NAO'. Você tem apenas %d tentativas".formatted(tentativas));
                     tentativas--;
                     continue;
                 } else if (viagemExterior.equalsIgnoreCase("SIM")){
                     probabilidadeInfeccao += 30;
                 }
+                // Dados do paciente
+                System.out.println("""
+                    Nome do paciente: %s
+                    Idade do paciente: %d
+                    Cartão de vacina: %s
+                    Sintomas: %s
+                    Contato com pessoas com sintomas gripais: %s
+                    Retorno de viagem realizada no exterior: %s
+                    Probabilidade de infecção: %d%%
+                    """.formatted(nome, idade, cartaoVacina, sintomas, contatoSintomas, viagemExterior, probabilidadeInfeccao));
                 // Calculo da probabilidade de infecção
                 if (probabilidadeInfeccao <= 30){
                     System.out.println("Paciente sob observação. Caso apareça algum sintoma, gentileza buscar assistência médica.");
                 } else if (probabilidadeInfeccao <=  60){
-                    System.out.println("Paciente com risco de estar infectado. Gentileza aguardar em lockdown por 02 dias para ser acompanhado.");
-                } else if (probabilidadeInfeccao >= 90){
+                    System.out.println("Paciente com risco de estar infectado. Gentileza aguardar em lockdown por 02 dias para ser acompanhado.");                    } else if (probabilidadeInfeccao >= 90){
                     System.out.println("Paciente crítico! Gentileza aguardar em lockdown por 10 dias para ser acompanhado.");
                 } else if (viagemExterior.equalsIgnoreCase("SIM")){
                     System.out.println("Você ficará sob observação por 05 dias.");
                 }
                 break;
+                }
+            if (tentativas == 0){
+                System.out.println("Não foi possível realizar o diagnóstico.\nGentileza procurar ajuda médica caso apareça algum sintoma.");
+                break;
+            }
+            System.out.print("Deseja realizar outro diagnóstico? (SIM/NAO): ");
+            String resposta = scanner.next();
+            if (resposta.equalsIgnoreCase("NAO")){
+                System.out.println("Programa encerrando.");
+                break;
+            } else if (!resposta.equalsIgnoreCase("SIM")){
+                System.out.println("Resposta inválida. O programa será encerrado.");
+                break;
             }
         }
+        scanner.close();
     }
 }
